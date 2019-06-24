ab93177a1303fe527fcf603a2b279dbe1a6b7f6a
Peter Werner <peternwerner@gmail.com>
2019-06-23 17:04:28 -0700

    roll library

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const Dice = require('Dice.js');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === '!roll') {
-    msg.reply(random());
-  }
+	var words = msg.toLowerCase().match(/\S+/g) || [];
+
+	if (words.length > 0 && words[0] == '!roll') {
+		if (words.length != 2) {
+			msg.reply("Invalid command: expected !roll <count>d<value>");
+		} else {
+			msg.reply(Dice.roll(words[1]));
+		}
+	}
+
 });
 
-function random() {
-  d= Math.random() * (20 - 1) + 1;
-  return parseInt(d, 10);
-}
-
 client.login('NTcwNzEzOTc3ODM1ODgwNDQ4.XRAFsA.jQI2_h2tS1PbT36jbshc_gSMdqU');
-
