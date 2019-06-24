5b9e64f56bca2210568c4d106fb1ee5a54649a65
Peter Werner <peternwerner@gmail.com>
2019-06-23 18:15:30 -0700

    data model start

---
+++ data_model_guide.js
@@ -0,0 +1,21 @@
+var character = {
+	id: 'unique_id_for_database',
+	userId: 'discord_id',
+	guildId: 'discord_channel_id',
+	stats: {
+		strength: 10,
+		dexterity: 10,
+		constitution: 10,
+		intelligence: 10,
+		wisdom: 10,
+		charisma: 10,
+	},
+	bonuses: {
+		strength: 2,
+		dexterity: 2,
+		constitution: 2,
+		intelligence: 2,
+		wisdom: 2,
+		charisma: 2,
+	},
+}
